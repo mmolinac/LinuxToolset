973dd0855fc09b660f8ebd4e7a9c219866710c7f
Manuel Molina Cuberos <mmc@pocosmhz.org>
2019-04-16 19:33:08 +0200

    VSCODE: Installed. Added apt-transport-https requirement, mostly for some Debian versions.

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 # Initial script to install Ansible and from there,
 # the rest of configuration and tools.
 
+echo "* Enter the SUDO password when required."
 chmod u+x $0
-echo "Installing Ansible..."
+echo "* Installing Ansible..."
 sudo apt-get -qq install ansible
-
+ansible-playbook -K playbooks/desktop-install-playbook.yml
