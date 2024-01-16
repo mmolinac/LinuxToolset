bb6052dd3dcbdf812c46b670da38ce860e50df8b
Manuel Molina Cuberos <mmc@pocosmhz.org>
2024-01-16 22:59:51 +0100

    Improved installation script.

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@
 
 echo "* Enter the SUDO password when required."
 chmod u+x $0
-echo "* Installing Ansible..."
-sudo apt-get -qq update
-sudo apt-get -qq install ansible
+which ansible > /dev/null
+if [ $? -ge 1 ]; then
+  echo "* Installing Ansible..."
+  sudo apt-get -qq update
+  sudo apt-get -qq install ansible
+fi
 ansible-playbook -K playbooks/desktop-install-playbook.yml
